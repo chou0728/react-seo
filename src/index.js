b13be04439d9fb04cc284869a9dbadaa183f3d9e
chou0728 <dean34520@gmail.com>
2019-05-19 21:58:18 +0800

    set up react-snap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import { render } from 'react-snapshot';
 
 const rootEl = document.getElementById('root');
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-, rootEl);
+  render(
+    <HashRouter>
+      <App/>
+    </HashRouter>, 
+    rootEl
+  )
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
